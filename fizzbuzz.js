bec5ed3c0663f87358749ef65b969fa69103cad2
Mathurah Ravigulan <mathurah.ravigulan@shopify.com>
2020-11-30 09:31:46 -0500

    added comments

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,13 +1,14 @@
 const number = process.argv[2]
-//creating if statements separating each case
+//Iniitialize a for loop that iterates from 1 up to the number.
 for (let i = 1; i <= number; i++)
-    if (i % 3 === 0 && i % 5 === 0) {
+//At every iteration, having a condition to check if its divisible by the following numbers
+    if (i % 3 === 0 && i % 5 === 0) { //if it is divisible by 3 and 5
         console.log('🛰', i)
     }
-    else if (i % 3 === 0) {
+    else if (i % 3 === 0) { // if it's divisible by 3
         console.log('👽', i);
-    } else if (i % 5 === 0) {
-        console.log('🚀', i);
-    } else {
+    } else if (i % 5 === 0) { //if it's divisible by 5
+        console.log('🚀', i); 
+    } else { //if it's not divisible by either
         console.log('💩', i);
     }
